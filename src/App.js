0160c7dec857b701faa2490a340213f56394e1fc
Greyland-TM <greyland.official@gmail.com>
2020-12-23 20:44:12 -0800

    update

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import './Components/LandingPage';
 import LandingPage from './Components/LandingPage';
 
 class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {activePage: 0}
-    }
-
-    togglePage(){
-        if (this.state.activePage === 0)
-            return(
-                <LandingPage></LandingPage>
-            )
-    }
-
     render(){
         return (
         <div className="demo-big-content">
@@ -26,16 +14,14 @@ class App extends Component{
                 <Header className="header-color" title="The Magnificant G" scroll>
                     <Navigation>
                         <Link to='/My-Portfolio'>Home</Link>
-                        {/* <Link to="/AboutMe">About Me</Link> */}
-                        <Link to="/Resume" activePage={1}>Resume</Link>
+                        <Link to="/Resume">Resume</Link>
                         <Link to="/Projects">Projects</Link>
                         <Link to="/Contact">Contact</Link>
                     </Navigation>
                 </Header>
                 <Drawer title="Title">
                     <Navigation>
-                        <Link to='/portfolio-app'>Home</Link>
-                        {/* <Link to="/About Me">About Me</Link> */}
+                        <Link to='/My-Portfolio'>Home</Link>
                         <Link to="/Resume">Resume</Link>
                         <Link to="/Projects">Projects</Link>
                         <Link to="/Contact">Contact</Link>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import LandingPage from './LandingPage';
-import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Resume from './Resume';
 import Contact from './Contact';
@@ -9,7 +8,6 @@ import Contact from './Contact';
 const Main = () => (
     <Switch>
         <Route exact path="/My-Portfolio" component={LandingPage} />
-        <Route path="/AboutMe" component={AboutMe} />
         <Route path="/Projects" component={Projects} />
         <Route path="/Resume" component={Resume} />
         <Route exact path="/Contact" component={Contact} />
